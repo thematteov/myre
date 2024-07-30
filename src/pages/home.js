9bf89aefd533c2fd6ebc72fe820863402a0d9f0d
thematteov <130756882+thematteov@users.noreply.github.com>
2024-07-30 13:33:25 +0200

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -63,8 +63,31 @@ function initHome() {
     { opacity: 1, stagger: 0.5 }
   );
 
-  //////////GENERAL//////////
-  //->videos
+  //////////PROJECTS//////////
+  let images = gsap.utils.toArray(".case");
+
+  images.forEach((el) => {
+    gsap.from(el.querySelector(".homearchiveimgwrapper"), {
+      scale: 0.3,
+      duration: 1.2,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: el,
+        start: "top bottom-=25%",
+        toggleActions: "play none none reverse",
+      },
+    });
+    gsap.from(el.querySelector(".videoThumb"), {
+      scale: 1.5,
+      duration: 1.2,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: el,
+        start: "top bottom-=10%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
 }
 gsap.registerPlugin(ScrollTrigger);
 export default initHome;
